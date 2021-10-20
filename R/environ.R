aed023ee4796a9afc7eacd5646dfee3eb2027c40
JeanneClement <jeanne.clement16@laposte.net>
2021-10-20 22:33:38 +0200

    Change jeanne

--- R/environ.R
+++ R/environ.R
@@ -27,7 +27,7 @@ if (length(arg)>0) {
 library(glue)
 library(here)
 library(sf)
-library(raster)
+library(stars)
 library(rgdal)
 library(rgrass7)
 
@@ -59,18 +59,19 @@ destfile <- here("data_raw", "srtm_v1_4_90m", "temp", "elevation.vrt")
 sourcefile <- here("data_raw", "srtm_v1_4_90m", "temp", "*.tif")
 system(glue("gdalbuildvrt {destfile} {sourcefile}"))
 
-## Reproject from lat long to UTM32N (epsg: 32622) (dstnodata need to be set to 32767 as we pass from Int16 (nodata=-32768) to INT2S (nodata=-32767) in R)
-border <- st_read(here("data_raw","fao_gaul","FAO_GAUL_GUY.kml"))
-border <- st_transform(border,crs=proj.t)
-destfile <- here("data_raw","fao_gaul","FAO_GAUL_GUY_epsg32622.kml")
+## Reproject from lat long to UTM32N (epsg: 32622) and reframe on French Guyana
+# (dstnodata need to be set to 32767 as we pass from Int16 (nodata=-32768) to INT2S (nodata=-32767) in R)
 sourcefile <- here("data_raw", "srtm_v1_4_90m", "temp", "elevation.vrt")
 destfile <- here("data_raw", "srtm_v1_4_90m", "temp", "elevation.tif")
 system(glue("gdalwarp -overwrite -s_srs {proj.s} -t_srs {proj.t} -srcnodata -32768 -dstnodata -32767 \\
         -r bilinear -tr 90 90 -te {Extent} -ot Int16 -of GTiff \\
         {sourcefile} \\
         {destfile}"))
-# plot(raster(here("data_raw", "srtm_v1_4_90m", "temp", "elevation.tif")))
-# plot(border$geometry, add=TRUE)
+# elev <- read_stars(here("data_raw", "srtm_v1_4_90m", "temp", "elevation.tif"))
+# border <- st_read(here("data_raw","fao_gaul","FAO_GAUL_GUY.kml"))
+# border <- st_transform(border,crs=st_crs(elev))
+# plot(elev,axes=TRUE,reset = FALSE)
+# plot(st_geometry(border), add=TRUE, reset=FALSE)
 
 ## Compute slope, aspect and roughness using gdaldem 
 # compute slope
@@ -86,7 +87,39 @@ system(cmd)
 out_f <- here("data_raw", "srtm_v1_4_90m", "temp", "roughness.tif")
 cmd <- glue('gdaldem roughness {in_f} {out_f} -co "COMPRESS=LZW" -co "PREDICTOR=2"')
 system(cmd)
-# resolution from 90m x 90m to 1000m x 1000m using gdalwarp
+
+#=== Compute radiation =====
+# with r.sun at 90m resolution 
+# Solar radiation (in Wh.m-2.day-1) was computed from altitude,
+# slope and aspect using the function r.sun from the GRASS GIS software.
+# We incorporated the shadowing effect of terrain to compute the solar radiation.
+# Solar radiation was computed for the Julian day 79 (20th of March for regular years=equinox).
+## Initialize GRASS
+setwd(here("data_raw"))
+Sys.setenv(LD_LIBRARY_PATH=paste("/usr/lib/grass76/lib", Sys.getenv("LD_LIBRARY_PATH"),sep=":"))
+# use a georeferenced raster
+elevation <- here("data_raw", "srtm_v1_4_90m", "temp", "elevation.tif")
+system(glue('grass -c {elevation} grassdata/environ'))
+# connect to grass database
+initGRASS(gisBase="/usr/lib/grass76", 
+          gisDbase="grassdata", home=tempdir(), 
+          location="environ", mapset="PERMANENT",
+          override=TRUE)
+## Import raster in grass
+system(glue("r.in.gdal --o input={elevation} output=elevation"))
+slope <- here("data_raw", "srtm_v1_4_90m", "temp", "slope.tif")
+system(glue("r.in.gdal --o input={slope} output=slope"))
+aspect <- here("data_raw", "srtm_v1_4_90m", "temp", "aspect.tif")
+system(glue("r.in.gdal --o input={aspect} output=aspect"))
+# Compute radiation
+cmd <- glue("r.sun --o --verbose elevation=elevation aspect=aspect slope=slope day=79 glob_rad=global_rad")
+system(cmd)
+# Export
+system(glue("r.out.gdal -f --overwrite input=global_rad \\
+  			 output={here('data_raw', 'srtm_v1_4_90m', 'temp', 'srad.tif')} type=Int16 nodata=-32767 \\
+  			 createopt='compress=lzw,predictor=2'"))
+
+# Resolution from 90m x 90m to 1000m x 1000m using gdalwarp
 # elevation
 out_f <- here("data_raw", "srtm_v1_4_90m", "elevation_1km.tif")
 cmd <- glue('gdalwarp -srcnodata -32767 -dstnodata -32767 -r bilinear -tr 1000 1000 -te {Extent} \\
@@ -95,27 +128,26 @@ system(cmd)
 # aspect
 in_f <- here("data_raw", "srtm_v1_4_90m", "temp", "aspect.tif")
 out_f <-here("data_raw", "srtm_v1_4_90m", "aspect_1km.tif")
-cmd <- glue('gdalwarp -srcnodata -9999 -dstnodata -32767 -r bilinear -tr 1000 1000 -te {Extent} \\
+cmd <- glue('gdalwarp -srcnodata {nodat} -dstnodata -32767 -r bilinear -tr 1000 1000 -te {Extent} \\
         -co "COMPRESS=LZW" -co "PREDICTOR=2" -overwrite {in_f} {out_f}')
 system(cmd)
 # slope
 in_f <- here("data_raw", "srtm_v1_4_90m", "temp", "slope.tif")
 out_f <- here("data_raw", "srtm_v1_4_90m", "slope_1km.tif")
-cmd <- glue('gdalwarp -srcnodata -9999 -dstnodata -32767 -r bilinear -tr 1000 1000 -te {Extent} \\
+cmd <- glue('gdalwarp -srcnodata {nodat} -dstnodata -32767 -r bilinear -tr 1000 1000 -te {Extent} \\
         -co "COMPRESS=LZW" -co "PREDICTOR=2" -overwrite {in_f} {out_f}')
 system(cmd)
 # roughness
 in_f <- here("data_raw", "srtm_v1_4_90m", "temp", "roughness.tif")
 out_f <- here("data_raw", "srtm_v1_4_90m", "roughness_1km.tif")
-cmd <- glue('gdalwarp -srcnodata -9999 -dstnodata -32767 \\
+cmd <- glue('gdalwarp -srcnodata {nodat} -dstnodata -32767 \\
         -r bilinear -tr 1000 1000 -te {Extent} -ot Int16 -of GTiff \\
         -co "COMPRESS=LZW" -co "PREDICTOR=2" -overwrite {in_f} {out_f}')
 system(cmd)
-
-# global radiation of wordclim or recompute ? 
-
-## Reproject
-# unzip
-# fichier vrt mosaicing 
-# gdalwarp
-# rm intermediaire files 
+# srad
+in_f <- here("data_raw", "srtm_v1_4_90m", "temp", "srad.tif")
+out_f <- here("data_raw", "srtm_v1_4_90m", "srad_1km.tif")
+cmd <- glue('gdalwarp -srcnodata -32767 -dstnodata -32767 -s_srs {proj.t} -t_srs {proj.t} \\
+        -r bilinear -tr 1000 1000 -te {Extent} -ot Int16 -of GTiff \\
+        -co "COMPRESS=LZW" -co "PREDICTOR=2" -overwrite {in_f} {out_f}')
+system(cmd)
