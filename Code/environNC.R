5ea1bf3161bf8360abc50d05ae53077f518ccc90
GuillaumontP <pierre.guillaumont@gmail.com>
2022-05-03 14:58:05 +0200

    environNC create one file for environ and soilgrids

--- Code/environNC.R
+++ Code/environNC.R
@@ -30,13 +30,82 @@ library(sf)
 library(stars)
 library(rgdal)
 library(rgrass7)
+library(tmaptools)
+library(terra)
 
 ## gdalwrap options
-Extent <- readLines(here("output/reExtent_short.txt"))
 Res <- "1000"
 nodat <- "-9999"
 proj.s <- "EPSG:4326"
 proj.t <- "EPSG:3165"
+ISO_country_code = "NCL"
+EPSG = 3165
+nodat = -9999
+proj.s <- "EPSG:4326"
+proj.t <- "EPSG:3165"
+
+
+##==============================
+##
+## Soilgrids
+##
+##==============================
+for (i in seq(163, 167, 2))
+{
+  for (j in seq(-23,-19, 2))
+  {
+    url = paste("https://maps.isric.org/mapserv?map=/map/wrb.map&SERVICE=WCS&VERSION=2.0.1&REQUEST=GetCoverage&COVERAGEID=MostProbable&FORMAT=image/tiff&SUBSET=long(",i, ".0000,", i+2, ".0000)&SUBSET=lat(", j, ".0000,", j+2, ".0000)&SUBSETTINGCRS=http://www.opengis.net/def/crs/EPSG/0/4326&OUTPUTCRS=http://www.opengis.net/def/crs/EPSG/0/4326", sep = "")
+    dest = here("data_raw", "soilgrids250_v2_0", paste("soilgrids_", j, "_", i, ".tif", sep = ""))
+    download.file(url = url, destfile = dest, verbose = TRUE)
+  }
+}
+sourcefile <- list.files(here("data_raw", "soilgrids250_v2_0"), pattern = ".tif", full.names = TRUE)
+merge19 <- c(read_stars(sourcefile[1]), read_stars(sourcefile[2]), read_stars(sourcefile[3]), along = 1)
+merge21 <- c(read_stars(sourcefile[4]), read_stars(sourcefile[5]), read_stars(sourcefile[6]), along = 1)
+merge23 <- c(read_stars(sourcefile[7]), read_stars(sourcefile[8]), read_stars(sourcefile[9]), along = 1)
+merge.tif <- c(merge19, merge21, merge23, along = 2)
+# plot(merge.tif, breaks = "equal", main = "NCL")
+write_stars(merge.tif, options = c("COMPRESS=LZW","PREDICTOR=2"), NA_value = nodat,
+            dsn = here("data_raw","soilgrids250_v2_0", "soilgrids_NoExtent.tif"))
+
+sourcefile <- here("data_raw", "soilgrids250_v2_0", "soilgrids_NoExtent.tif")
+destfile <- here("data_raw", "soilgrids250_v2_0", "soilgrids_1km.tif")
+system(glue("gdalwarp -overwrite -s_srs {proj.s} -t_srs {proj.t} \\
+        -r bilinear -tr 1000 1000 -ot Int16 -srcnodata {nodat} -of GTiff \\
+        {sourcefile} \\
+        {destfile}"))
+read_stars(destfile)
+plot(read_stars(destfile), breaks = "equal") # ferralsol == ultramafic
+
+#Redimension others tif files
+soilgrids = read_stars(here("data_raw", "soilgrids250_v2_0", "soilgrids_1km.tif"))
+d = st_dimensions(soilgrids)
+offset = c(d[["x"]]$offset, d[["y"]]$offset)
+reExtent = paste0(c(xmin = round(offset[1]),
+                    ymin = round(offset[2]),
+                    xmax = round(offset[1] + d[["x"]]$to * d[["x"]]$delta),
+                    ymax = round(offset[2] - d[["y"]]$to * d[["y"]]$delta)), collapse = " ")
+writeLines(reExtent, here("output", "reExtent_short.txt"))
+
+URL <- paste0("https://geodata.ucdavis.edu/gadm/gadm3.6/gpkg/gadm36_", ISO_country_code, "_gpkg.zip")
+# Download file
+# The coordinate reference system is longitude/latitude and the WGS84 datum.
+download.file(URL, quiet = FALSE, 
+              here("data_raw", "fao_gaul", paste0("gpkg_gadm36_", ISO_country_code, ".zip")))
+# Unzip 
+unzip(here("data_raw", "fao_gaul", paste0("gpkg_gadm36_", ISO_country_code, ".zip")),
+      exdir=here("data_raw", "fao_gaul"), overwrite=TRUE)
+# Read vector (level 0 for country borders)
+border <- sf::st_read(here("data_raw", "fao_gaul", paste0("gadm36_", ISO_country_code, ".gpkg")),
+                      layer=paste0("gadm36_", ISO_country_code, "_0"), quiet=FALSE)
+border <- st_transform(border[1], crs = EPSG)
+border <- st_crop(border, st_bbox(soilgrids))
+soilgrids <- st_crop(soilgrids, border)
+write_stars(soilgrids, options = c("COMPRESS=LZW","PREDICTOR=2"), NA_value = nodat,
+            dsn = here("data_raw", "soilgrids250_v2_0", "soilgrids_1km.tif"), layer = 1)
+soilgrids <- read_stars(here("data_raw", "soilgrids250_v2_0", "soilgrids_1km.tif"))
+
+
 
 #====== Elevation, slope aspect, roughness #======
 # SRTM at 90m resolution from https://dwtkns.com/srtm/ version 4.1
@@ -159,6 +228,55 @@ cmd <- glue('gdalwarp -s_srs {proj.t} -t_srs {proj.t} \\
         -r bilinear -tr 1000 1000 -ot Int16 -of GTiff \\
         -co "COMPRESS=LZW" -co "PREDICTOR=2" -overwrite {in_f} {out_f}')
 system(cmd)
+
+##=====================================
+##
+## Merge environmental variables in one .tif
+##
+##=====================================
+
+aspect <- read_stars(here("data_raw", "srtm_v1_4_90m", "aspect_1km.tif"))
+elevation <- read_stars(here("data_raw", "srtm_v1_4_90m", "elevation_1km.tif"))
+roughness <- read_stars(here("data_raw", "srtm_v1_4_90m", "roughness_1km.tif"))
+slope <- read_stars(here("data_raw", "srtm_v1_4_90m", "slope_1km.tif"))
+srad <- read_stars(here("data_raw", "srtm_v1_4_90m", "srad_1km.tif"))
+soilgrids <- read_stars(here("data_raw", "soilgrids250_v2_0", "soilgrids_1km.tif"))
+
+borders <- st_bbox(aspect)
+aspect <- st_normalize(st_crop(aspect,borders))
+elevation <- st_normalize(st_crop(elevation, borders))
+roughness <- st_normalize(st_crop(roughness, borders))
+slope <- st_normalize(st_crop(slope, borders))
+srad <- st_normalize(st_crop(srad, borders))
+soilgrids <- st_normalize(st_crop(soilgrids, borders))
+
+st_dimensions(aspect)[["x"]]$offset = round(borders$xmin)
+st_dimensions(aspect)[["y"]]$offset = round(borders$ymax)
+st_dimensions(elevation)[["x"]]$offset = round(borders$xmin)
+st_dimensions(elevation)[["y"]]$offset = round(borders$ymax)
+st_dimensions(roughness)[["x"]]$offset = round(borders$xmin)
+st_dimensions(roughness)[["y"]]$offset = round(borders$ymax)
+st_dimensions(slope)[["x"]]$offset = round(borders$xmin)
+st_dimensions(slope)[["y"]]$offset = round(borders$ymax)
+st_dimensions(srad)[["x"]]$offset = round(borders$xmin)
+st_dimensions(srad)[["y"]]$offset = round(borders$ymax)
+st_dimensions(soilgrids)[["x"]]$offset = round(borders$xmin)
+st_dimensions(soilgrids)[["y"]]$offset = round(borders$ymax)
+
+x_to <- min(st_dimensions(aspect)[['x']]$to, st_dimensions(soilgrids)[['x']]$to, st_dimensions(srad)[['x']]$to)
+y_to <- min(st_dimensions(aspect)[['y']]$to, st_dimensions(soilgrids)[['y']]$to, st_dimensions(srad)[['y']]$to)
+
+aspect <- st_normalize(aspect[, 1 + (st_dimensions(aspect)[['x']]$to - x_to) : x_to - 1, 1 + (st_dimensions(aspect)[['y']]$to - y_to) : y_to - 1])
+elevation <- st_normalize(elevation[, 1 + (st_dimensions(elevation)[['x']]$to - x_to) : x_to - 1, 1 + (st_dimensions(elevation)[['y']]$to - y_to) : y_to - 1])
+roughness <- st_normalize(roughness[, 1 + (st_dimensions(roughness)[['x']]$to - x_to) : x_to - 1, 1 + (st_dimensions(roughness)[['y']]$to - y_to) : y_to - 1])
+slope <- st_normalize(slope[, 1 + (st_dimensions(slope)[['x']]$to - x_to) : x_to - 1, 1 + (st_dimensions(slope)[['y']]$to - y_to) : y_to - 1])
+srad <- st_normalize(srad[, 1 + (st_dimensions(srad)[['x']]$to - x_to) : x_to - 1, 1 + (st_dimensions(srad)[['y']]$to - y_to) : y_to - 1])
+soilgrids <- st_normalize(soilgrids[, 1 + (st_dimensions(soilgrids)[['x']]$to - x_to) : x_to - 1, 1 + (st_dimensions(soilgrids)[['y']]$to - y_to) : y_to - 1])
+
+r <- c(aspect, elevation, roughness, slope, srad, soilgrids, along = "band", try_hard = TRUE)
+write_stars(r, options = c("COMPRESS=LZW","PREDICTOR=2"), NA_value = nodat,
+            dsn = here("data_raw", "srtm_v1_4_90m", "environ.tif"))
+
 
 ##==========================================================
 ##
